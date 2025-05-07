9d526f26b998aab5d8b0c6b55da323b46be49693
MASTERALVANO <masteralvano@gmail.com>
2025-05-07 21:08:19 +0700

    percobaan 1-3 Finish
    Selesai yang jelek

--- JS/script.js
+++ JS/script.js
@@ -8,30 +8,57 @@ var text = [
 ];
 var judul = ["Mango Thai", "Avocado Smoothies", "Strawberry Smoothies", "Ice Chocoblend With Oreo"];
 
-document.getElementById("mango").addEventListener("click", function () {
-    currentRotation = (currentRotation + 1) % rotate.length;
-    document.getElementsByClassName("roll")[0].style.transform = "rotate(" + rotate[0] + "deg)";
-    document.querySelector(".content p").innerText = text[0];
-    document.querySelector(".content h3").innerText = judul[0];
+document.addEventListener("DOMContentLoaded", function () {
+    const preview = document.querySelector("img[name='preview']");
+    const leftButton = document.getElementById("left");
+    const rightButton = document.getElementById("right");
+
+    if (leftButton && rightButton && preview) {
+        leftButton.addEventListener("click", function () {
+            imageIndex--;
+            if (imageIndex < 0) imageIndex = image.length - 1;
+            preview.src = image[imageIndex];
+        });
+
+        rightButton.addEventListener("click", function () {
+            imageIndex++;
+            if (imageIndex >= image.length) imageIndex = 0;
+            preview.src = image[imageIndex];
+        });
+    }
+
+    document.getElementById("mango")?.addEventListener("click", function () {
+        currentRotation = (currentRotation + 1) % rotate.length;
+        document.querySelector(".roll")?.style.setProperty("transform", `rotate(${rotate[0]}deg)`);
+        document.querySelector(".content p").innerText = text[0];
+        document.querySelector(".content h3").innerText = judul[0];
+    });
+
+    document.getElementById("avocado")?.addEventListener("click", function () {
+        currentRotation = (currentRotation + 1) % rotate.length;
+        document.querySelector(".roll")?.style.setProperty("transform", `rotate(${rotate[1]}deg)`);
+        document.querySelector(".content p").innerText = text[1];
+        document.querySelector(".content h3").innerText = judul[1];
+    });
+
+    document.getElementById("berry")?.addEventListener("click", function () {
+        currentRotation = (currentRotation + 1) % rotate.length;
+        document.querySelector(".roll")?.style.setProperty("transform", `rotate(${rotate[2]}deg)`);
+        document.querySelector(".content p").innerText = text[2];
+        document.querySelector(".content h3").innerText = judul[2];
+    });
+
+    document.getElementById("oreo")?.addEventListener("click", function () {
+        currentRotation = (currentRotation + 1) % rotate.length;
+        document.querySelector(".roll")?.style.setProperty("transform", `rotate(${rotate[3]}deg)`);
+        document.querySelector(".content p").innerText = text[3];
+        document.querySelector(".content h3").innerText = judul[3];
+    });
 });
 
-document.getElementById("avocado").addEventListener("click", function () {
-    currentRotation = (currentRotation + 1) % rotate.length;
-    document.getElementsByClassName("roll")[0].style.transform = "rotate(" + rotate[1] + "deg)";
-    document.querySelector(".content p").innerText = text[1];
-    document.querySelector(".content h3").innerText = judul[1];
-});
-
-document.getElementById("berry").addEventListener("click", function () {
-    currentRotation = (currentRotation + 1) % rotate.length;
-    document.getElementsByClassName("roll")[0].style.transform = "rotate(" + rotate[2] + "deg)";
-    document.querySelector(".content p").innerText = text[2];
-    document.querySelector(".content h3").innerText = judul[2];
-});
-
-document.getElementById("oreo").addEventListener("click", function () {
-    currentRotation = (currentRotation + 1) % rotate.length;
-    document.getElementsByClassName("roll")[0].style.transform = "rotate(" + rotate[3] + "deg)";
-    document.querySelector(".content p").innerText = text[3];
-    document.querySelector(".content h3").innerText = judul[3];
-});
+var image = [
+    "resource/Frame 15.png",
+    "resource/Frame 15 (1).png",
+    "resource/Frame 15 (2).png"
+];
+var imageIndex = 0;
